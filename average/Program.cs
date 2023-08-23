fccd86da95971d7caaf8d068e076f18c8f66be57
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-08-23 09:25:38 +0530

    Add project files.

---
+++ average/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace average_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double sum = 0;
+            int n;
+            Console.WriteLine("Enter the size of array:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Double[] a = new Double[n];
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine("Enter element {0}:", i + 1);
+                a[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+                Console.WriteLine("Average:" + (sum/n));
+                Console.ReadLine();
+            }
+        }
+  
+}
